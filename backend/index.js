ce2f7ad5e3449d1407452dc9f53332283e2b3e02
Ali_Moghaddam <se.ali.moghaddam@gmail.com>
2024-08-13 04:12:25 -0700

    Database Added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
-const express = require('express');
+import express from 'express';
+import dbConnect from './config/db/dbConnect.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
+dbConnect();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
-console.log(`The server is running on port(${PORT}) :)`);
+app.listen(PORT, console.log(`The server is running on port(${PORT}) :)`));
