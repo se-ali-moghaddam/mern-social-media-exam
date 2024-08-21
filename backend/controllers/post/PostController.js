4e3ebeb266caa131480f0ba0dee0567075a93ad6
Ali_Moghaddam <se.ali.moghaddam@gmail.com>
2024-08-21 05:46:30 -0700

    Post functionalities completed

--- backend/controllers/post/PostController.js
+++ backend/controllers/post/PostController.js
@@ -39,5 +39,76 @@ export const postUpdate = asyncHandler(async (req, res) => {
 
     await Post.findByIdAndUpdate(id, req.body, {new: true});
 
-    res.json("The post updated succesfully :)");
+    res.json('The post updated succesfully :)');
+});
+
+export const postDelete = asyncHandler(async (req, res) => {
+    const {id} = req?.params;
+    validateMongoDbId(id);
+
+    await Post.findByIdAndDelete(id);
+
+    res.json('The post removed succesfully');
+});
+
+export const postToggleLike = asyncHandler(async (req, res) => {
+    const {id} = req?.params;
+    validateMongoDbId(id);
+
+    const loginUserId = req?.userId;
+    const post = await Post.findById(id);
+
+    if(post.dislikes.find(userId => userId.toString() === loginUserId.toString())){
+        await Post.findByIdAndUpdate(id, {
+            $pull: {dislikes: loginUserId},
+            isDisliked: false
+        });
+    }
+
+    if(post.isLiked){
+        await Post.findByIdAndUpdate(id, {
+            $pull: {likes: loginUserId},
+            isLiked: false
+        });
+
+        res.json('The post was unliked successfully !');
+    } else {
+        await Post.findByIdAndUpdate(id, {
+            $push: {likes: loginUserId},
+            isLiked: true
+        });
+
+        res.json('The post was liked successfully !');
+    }
+});
+
+export const postToggleDislike = asyncHandler(async (req, res) => {
+    const {id} = req?.params;
+    validateMongoDbId(id);
+
+    const loginUserId = req?.userId;
+    const post = await Post.findById(id);
+
+    if(post.likes.find(userId => userId.toString() === loginUserId.toString())){
+        await Post.findByIdAndUpdate(id, {
+            $pull: {likes: loginUserId},
+            isLiked: false
+        });
+    }
+
+    if(post.isDisliked){
+        await Post.findByIdAndUpdate(id, {
+            $pull: {dislikes: loginUserId},
+            isDisliked: false
+        });
+
+        res.json('The post was undisliked successfully !');
+    } else {
+        await Post.findByIdAndUpdate(id, {
+            $push: {dislikes: loginUserId},
+            isDisliked: true
+        });
+
+        res.json('The post was disliked successfully !');
+    }
 });
